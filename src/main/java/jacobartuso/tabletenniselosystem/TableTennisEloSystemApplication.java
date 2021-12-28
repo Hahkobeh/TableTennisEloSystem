cb7a227a8f7eae9bff96fdd6a86a76e4c76ce3d3
Jacob <hahkobeh@gmail.com>
2021-12-28 14:11:54 -0700

    Initial commit

---
+++ src/main/java/jacobartuso/tabletenniselosystem/TableTennisEloSystemApplication.java
@@ -0,0 +1,13 @@
+package jacobartuso.tabletenniselosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableTennisEloSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableTennisEloSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jacobartuso/tabletenniselosystem/TableTennisEloSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jacobartuso.tabletenniselosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTennisEloSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
